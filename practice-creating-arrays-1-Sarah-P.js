cb02ac33e6287819148d90e90f1dcfbbda8564bd
Sarah <skat1403@gmail.com>
2025-03-31 19:01:08 -0500

    created arrays

--- practice-creating-arrays-1-Sarah-P.js
+++ practice-creating-arrays-1-Sarah-P.js
@@ -1,1 +1,23 @@
+/*
+Tasks
+Task 1: Create Arrays
+Create three separate arrays using array literals for the following:
+● An array of 5 grocery store items.
+● An array of 3 numbers representing prices.
+Task 2: Access and Log the Values
+Use console.log() to log the arrays.
+1. Log the entire array of grocery items.
+2. Log the entire array of prices.
+3. Log three individual items of your choice.
+4. Log two prices of your choice.
+*/
 
+let storeItems = ["apple", "milk", "eggs", "bread", "coffee"];
+let priceList = [1.99, 3.50, 7.16];
+let thirdArray = [1, "orange", "blanket", 84];
+
+
+console.log(storeItems);
+console.log(priceList);
+console.log(storeItems[0], storeItems[1]);
+console.log(priceList[2], priceList[0]);
